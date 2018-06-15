806222bba5a7e828b36497036631583fe44688a0
Lurtt <lurttik@gmail.com>
2018-06-15 08:05:58 +0200

    simple query example

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-// 1
 const typeDefs = `
 type Query {
   info: String!
+  feed: [Link!]!
+}
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
 }
 `
+let links = [{
+  id: 'link-0',
+  url: 'www.supercoollink.com',
+  description: 'some cool description'
+}]
 
-// 2
 const resolvers = {
   Query: {
-    info: () => `This is the API of the app`
+    info: () => `This is the API of the app`,
+    feed: () => links,
+  },
+
+  Link: {
+    id: ({ id }) => id,
+    url: ({ url }) => url,
+    description: ({ description }) => description,
   }
 }
 
