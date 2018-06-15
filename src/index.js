0de07b5c6eeb016f8b81f553f81dedb17f058993
Lukas Pospisil <lukas.pospisil@pingdom.com>
2018-06-15 12:34:00 +0200

    connecting server and database with prisma bindings

--- src/index.js
+++ src/index.js
@@ -1,35 +1,37 @@
 const { GraphQLServer } = require('graphql-yoga')
-
-let links = [{
-  id: 'link-0',
-  url: 'www.supercoollink.com',
-  description: 'some cool description'
-}]
-
-let idCount = links.length
+const { Prisma } = require('prisma-binding')
 
 const resolvers = {
   Query: {
     info: () => `This is the API of the app`,
-    feed: () => links,
+    feed: (root, args, context, info) => {
+      return context.db.query.links({}, info)
+    },
   },
-
   Mutation: {
-    post: (root, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      }
-      links.push(link)
-      return link
-    }
+    post: (root, args, context, info) => {
+      return context.db.mutation.createLink({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      }, info)
+    },
   },
 }
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers
+  resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql',
+      endpoint: 'https://eu1.prisma.sh/lukas-pospisil-2c7306/games-node/dev',
+      secret: 'mytotalbrutalimportantsecretomgwtfbbq',
+      debug: true,
+    }),
+  }),
 })
 
 server.start(() => console.log(`Server is running on http://localhost:4000`))
