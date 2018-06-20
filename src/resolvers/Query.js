92b27836fba5805f894441067499a6ea0c61bf53
Lukas Pospisil <lukas.pospisil@pingdom.com>
2018-06-20 09:07:34 +0200

    added app schema Feed type

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,4 +1,4 @@
-function feed(root, args, context, info) {
+async function feed(parent, args, context, info) {
   const where = args.filter
     ? {
       OR: [
@@ -8,10 +8,25 @@ function feed(root, args, context, info) {
     }
     : {}
 
-  return context.db.query.links(
+  const queriedLinks = await context.db.query.links(
     { where, skip: args.skip, first: args.first, orderBy: args.orderBy },
-    info,
+    `{ id }`,
   )
+
+  const countSelectionSet = `
+    {
+      aggregate {
+        count
+      }
+    }
+  `
+
+  const linksConnection = await context.db.query.linksConnection({}, countSelectionSet)
+
+  return {
+    count: linksConnection.aggregate.count,
+    linkIds: queriedLinks.map(link => link.id),
+  }
 }
 
 module.exports = {
